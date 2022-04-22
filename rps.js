df49f9586ddc7341ba197c351bbd16a0cda15554
Raniel Quesada <ranielquesada@Raniels-MacBook-Pro.local>
2022-04-21 18:57:56 -0700

    Finalized game mechanics

--- rps.js
+++ rps.js
@@ -1,34 +1,59 @@
-function playRound(playerSelection, computerSelection){
+let playerSelection = "action";
+let computerSelection = "action";
+const computerActions = ["rock", "paper", "scissors"];
+
+function results(playerSelection, computerSelection){
     
-    if(playerSelection === "rock" && computerSelection === "scissor") {
+    if(playerSelection === "rock" && computerSelection === "scissors") {
         return alert("You win! Rock beats scissors")
     }
     else if(playerSelection === "rock" && computerSelection === "paper") {
         return alert("You lose! Paper beats rock")
     }
-    else if(playerSelection === "rock" && computerSelection === "rock") {
-        return alert("Tie!")
+    else if(playerSelection === "paper" && computerSelection === "scissors") {
+        return alert("You lose! Scissors beats paper")
+    }
+    else if(playerSelection === "paper" && computerSelection === "rock") {
+        return alert("You Win! Paper beats rock")
+    }
+    else if(playerSelection === "scissors" && computerSelection === "rock") {
+        return alert("You lose! Rock beats scissors")
+    }
+    else if(playerSelection === "scissors" && computerSelection === "paper") {
+        return alert("You win! Scissors beats paper")
+    }
+    else {
+        return alert("Tie")
     }
 
 }
 
-function computerPlay(){
-    return "scissor";
+function computerAction(){
+    const randomIndex = Math.floor(Math.random() * computerActions.length);
+    return computerActions[randomIndex];
 }
-let playerSelection = "action";
 
 function pickAction(){
-    playerSelection = prompt("Please enter rock, paper, or scissor");
+    playerSelection = prompt("Please enter rock, paper, or scissors")
+    if(playerSelection != null){
+        playerSelection.toLowerCase();
+    }
+    return playerSelection;
 }
-
-
-
 
 
 function playGame(){
-    const computerSelection = computerPlay();
-    const playerSelection = prompt("Please enter rock, paper, or scissor");
-    playRound(playerSelection, computerSelection)
+    
+    playerSelection = pickAction();
+    if(playerSelection != "rock" || playerSelection === "paper" || playerSelection === "scissors"){
+        alert("You didnt type your action or you mispelled it!")
+    }
+    else{
+    computerSelection = computerAction();
+    alert("Computer picks " + computerSelection)
+    
+    results(playerSelection, computerSelection)
+    }
 
 }
 
